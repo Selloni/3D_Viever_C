5f6d102b5f28565ccbc22a5dc4f331a1351e552d
Grandpa Trula <grandpat@ev-e4.kzn.21-school.ru>
2022-12-05 15:42:46 +0300

    пунктирная_линия_и_рамер

--- 3DWieve/scene.cpp
+++ 3DWieve/scene.cpp
@@ -7,24 +7,28 @@ Scene::Scene(QWidget *parent):
 }
 
 void Scene::initializeGL() {
+    glEnable(GL_DEPTH_TEST); // буфер клубины
     glClearColor( 0.0f, 0.0f, 0.f, 0.0f );  //  colo bakcground
 }
-float arr[] = {0, 0, -0.5, 1, 0, -1, 0, 1, -1}; // масив вершин
+float arr[] = {0,0,0, -1,0,-1, 0,1,0, 1,0,0}; // масив вершин
+
 void Scene::paintGL() {
 
-    glClear(GL_COLOR_BUFFER_BIT);
-//    glVertexPointer(3, GL_FLOAT, 0, &arr);  // берет каждые три точки под вершины
-//    glEnableClientState(GL_VERTEX_ARRAY);
-//        glColor3d(1,0,1);
-//        glDrawArrays(GL_VERTEX_ARRAY,0,3);
-//    glDisableClientState(GL_VERTEX_ARRAY);
-    glBegin(GL_TRIANGLES);
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    glVertexPointer(3, GL_FLOAT, 0, &arr);  // берет каждые три точки под вершины
+    glEnableClientState(GL_VERTEX_ARRAY);
 
-    glVertex3d(0, 0, -0.5);
-    glVertex3d(1, 0, -1);
-    glVertex3d(0, 1, -1);
-    glEnd();
+        glColor3d(1,0,1);  //  color
 
+        glEnable(GL_LINE_STIPPLE); // пунктирная линия
+        glLineStipple(1, 0x00ff);
+
+        glLineWidth(6); // size line
+        glDrawArrays(GL_LINE_LOOP,0,4);
+        glPointSize(16);  // size point
+
+        glDrawArrays(GL_POINTS, 0, 3);
+    glDisableClientState(GL_VERTEX_ARRAY);
 }
 
 void Scene::resizGL( int w, int h) {
@@ -32,21 +36,21 @@ void Scene::resizGL( int w, int h) {
     glViewport( 0, 0, w, h );
 
     // Создаем проекцию
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
+    glMatrixMode(GL_PROJECTION);  // ортоганальая поекция
+    glLoadIdentity(); // загружаем матрицу
     // Establish clipping volume (left, right, bottom, top, near, far)
-//    glOrtho(-1, 1, -1, 1, 1, 2);
-    glFrustum(-1, 1, -1, 1, 1, 2);
+    glFrustum(-10, 10, -10, 10, 10, 10);  //  перспективная проекция
 
-
-
-//    // Reset coordinate system
-//    glMatrixMode( GL_PROJECTION );
-//    glLoadIdentity();
-
-//    // Establish clipping volume (left, right, bottom, top, near, far)
-//    glOrtho( -200, 200, -200, 200, 1.0, -1.0 );
-
-//    glMatrixMode( GL_MODELVIEW );
-//    glLoadIdentity();
 }
+
+void Scene::mousePressEvent(QMouseEvent* mo) {
+    mPos = mo->pos();
+}
+
+void Scene::mouseMoveEvent(QMouseEvent* mo) {
+    xRot = 1/M_PI*(mo->pos().y() - mPos.y());
+    xRot = 1/M_PI*(mo->pos().x() - mPos.x());
+    update(); //обовление кординат
+}
+
+
--- 3DWieve/scene.h
+++ 3DWieve/scene.h
@@ -2,19 +2,27 @@
 #define SCENE_H
 
 #include <QWidget>
+//#include <QGLWidget>
 #include <QtOpenGL>
 #include <QOpenGLWidget>
 #include <QOpenGLShaderProgram>
-#include <QMatrix4x4>
+//#include <QMatrix4x4>
 
 class Scene: public QOpenGLWidget
 {
-public:
-    Scene(QWidget *parent = 0);
+private slots:
+    float xRot, yRot, zRot;
+    QPoint mPos;
+    void mousePressEvent(QMouseEvent*); // click mouse
+    void mouseMoveEvent(QMouseEvent*); // move mouse
 
     void initializeGL();  // вызываеться после вызова конструктора
     void paintGL();  // нужно перерасовть окно
     void resizGL( int w, int h);  //  когда изменяеться размер окна
+public:
+    Scene(QWidget *parent = 0);
+
+
 
 };
 
